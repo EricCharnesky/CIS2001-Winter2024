4ffec4130096289697d21614cf68553ef914df77
Eric Charnesky <EricCharnesky@gmail.com>
2024-03-28 15:26:05 -0400

    finish BST

--- Weel12-BinarySearchTrees/main.py
+++ Weel12-BinarySearchTrees/main.py
@@ -32,21 +32,71 @@ class BinarySearchTree:
         self._insert(data, self._root)
 
     def _contains(self, data, node):
+        if node is None:
+            return False
         if data == node.data:
             return True
         if data < node.data:
-            if node.left_child is None:
-                return False
             return self._contains(data, node.left_child)
-        if node.right_child in None:
-            return False
         return self._contains(data, node.right_child)
 
     # assuming we have somewhat of a balanced structure, we get O(log(n))
     def contains(self, data):
         return self._contains(data)
 
+
+    def _swap_node(self, node, node_to_swap):
+        parent = node.parent
+        if parent.left_child == node:
+            parent.left_child = node_to_swap
+        else:
+            parent.right_child = node_to_swap
+        if node_to_swap is not None:
+            node_to_swap.parent = parent
+
+    def _remove(self, data, node):
+        if node is None:
+            raise ValueError("Not Found")
+        if data == node.data:
+            next_largest_smaller_node = node.left_child
+            if next_largest_smaller_node is None:
+                self._swap_node(node, node.right_child)
+            else:
+                while next_largest_smaller_node.right_child is not None:
+                    next_largest_smaller_node = next_largest_smaller_node.right_child
+                value = next_largest_smaller_node.data
+                self._swap_node(next_largest_smaller_node, next_largest_smaller_node.left_child)
+                node.data = value
+
+        if data < node.data:
+            return self._remove(data, node.left_child)
+        else:
+            return self._remove(data, node.right_child)
+
+
     # assuming we have somewhat of a full structure, we get O(log(n))
     def remove(self, data):
         # find the data, then replace with the largest smaller item
         # left child then all the way right
+        self._remove(data, self._root)
+
+    def _print_in_order(self, node):
+        if node.left_child:
+            self._print_in_order(node.left_child)
+        print(node.data)
+        if node.right_child:
+            self._print_in_order(node.right_child)
+    def print_in_order(self):
+        self._print_in_order(self._root)
+
+
+values = BinarySearchTree(7)
+values.insert(2)
+values.insert(6)
+values.insert(5)
+values.insert(0)
+values.insert(1)
+values.insert(11)
+values.insert(9)
+
+values.print_in_order()
